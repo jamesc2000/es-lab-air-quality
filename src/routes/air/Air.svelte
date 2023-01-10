b90072bd7bc9acc8dd5f0ce6321ad985ed32d583
James <cruz.james99@gmail.com>
2023-01-10 16:18:29 +0800

    + Add firebase hosting + Run prettier on Air.svelte

--- src/routes/air/Air.svelte
+++ src/routes/air/Air.svelte
@@ -2,7 +2,7 @@
 	// Everytime na gusto natin gamitin si chartjs for a component, eto eto mga need i import
 	// tas need din sila i-register for some reason di q alam bakit haha
 	import { Chart } from 'svelte-chartjs';
-  import { onMount, onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import annotationPlugin from 'chartjs-plugin-annotation';
 	import {
 		Chart as ChartJS,
@@ -13,17 +13,25 @@
 		LineElement,
 		LinearScale,
 		PointElement,
-    TimeScale,
-    TimeSeriesScale,
-    LineController,
-    Decimation
+		TimeScale,
+		TimeSeriesScale,
+		LineController,
+		Decimation
 	} from 'chart.js';
 
-	import { getDatabase, onChildAdded, orderByKey, ref, query, off, limitToLast } from 'firebase/database';
+	import {
+		getDatabase,
+		onChildAdded,
+		orderByKey,
+		ref,
+		query,
+		off,
+		limitToLast
+	} from 'firebase/database';
 	import { app } from '../../util/initFirebase';
-  import { transformData } from './dataHelpers';
-  import moment from "moment";
-  import "chartjs-adapter-moment";
+	import { transformData } from './dataHelpers';
+	import moment from 'moment';
+	import 'chartjs-adapter-moment';
 
 	ChartJS.register(
 		Filler,
@@ -34,44 +42,44 @@
 		LineElement,
 		LinearScale,
 		PointElement,
-    TimeScale,
-    TimeSeriesScale,
-    LineController,
-    Decimation
+		TimeScale,
+		TimeSeriesScale,
+		LineController,
+		Decimation
 	);
 
 	// Etong testData parang mock data lang cinopy paste q lang dito haha
 	// https://codesandbox.io/s/github/SauravKanchan/svelte-chartjs/tree/master/sandboxes/line
 
-  let chart;
+	let chart;
 	// const xlabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August']; //to change
 	// const ylabels = []; //to change
 
-  let mockData = {
-    data: [
-      {
-        x: moment().unix() * 1000,
-        y: 0
-      }
-    ]
-  };
+	let mockData = {
+		data: [
+			{
+				x: moment().unix() * 1000,
+				y: 0
+			}
+		]
+	};
 
-  function mockDataUpdates() {
-    setTimeout(() => {
-      chart.data.datasets[0].data.push({
-        y: Math.random() * 50,
-        x: moment().unix() * 1000
-      });
-      // mockData.data = temp;
-      // console.log(mychart);
-      // console.log(chart);
-      chart.update();
+	function mockDataUpdates() {
+		setTimeout(() => {
+			chart.data.datasets[0].data.push({
+				y: Math.random() * 50,
+				x: moment().unix() * 1000
+			});
+			// mockData.data = temp;
+			// console.log(mychart);
+			// console.log(chart);
+			chart.update();
 
-      console.log("Push data");
-      console.log(mockData.data);
-      mockDataUpdates();
-    }, 5000);
-  }
+			console.log('Push data');
+			console.log(mockData.data);
+			mockDataUpdates();
+		}, 5000);
+	}
 
 	let chartData = {
 		datasets: [
@@ -101,26 +109,26 @@
 
 	const options = {
 		responsive: true,
-    maintainAspectRatio: false,
-    parsing: false,
-    scales: {
-      x: {
-        type: 'timeseries',
-        ticks: {
-          autoSkip: true,
-          maxTicksLimit: 8
-        },
-        time: {
-          round: "minute"
-        }
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'value'
-        }
-      }
-    },
+		maintainAspectRatio: false,
+		parsing: false,
+		scales: {
+			x: {
+				type: 'timeseries',
+				ticks: {
+					autoSkip: true,
+					maxTicksLimit: 8
+				},
+				time: {
+					round: 'minute'
+				}
+			},
+			y: {
+				title: {
+					display: true,
+					text: 'value'
+				}
+			}
+		},
 		plugins: {
 			autocolors: true,
 			annotation: {
@@ -139,11 +147,12 @@
 					}
 				}
 			},
-      decimation: { // TODO: Decimation not working, pero kung ayaw talaga wag nalang haha
-        enabled: true,
-        agorithm: 'lltb',
-        samples: 10
-      }
+			decimation: {
+				// TODO: Decimation not working, pero kung ayaw talaga wag nalang haha
+				enabled: true,
+				agorithm: 'lltb',
+				samples: 10
+			}
 		}
 	};
 
@@ -151,34 +160,33 @@
 
 	const airQualityDataRef = query(ref(db, 'devices/james-esp32'), orderByKey(), limitToLast(100));
 
-  function listenToDb() {
-    onChildAdded(airQualityDataRef, (res) => {
-      console.log(chart.data.datasets[0].data.length);
-      try {
-        chart.data.datasets[0].data.push(transformData(res.val()));
-        chart.update();
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
+	function listenToDb() {
+		onChildAdded(airQualityDataRef, (res) => {
+			console.log(chart.data.datasets[0].data.length);
+			try {
+				chart.data.datasets[0].data.push(transformData(res.val()));
+				chart.update();
+			} catch (error) {
+				console.error(error);
+			}
+		});
+	}
 
-  listenToDb();
-  onMount(() => {
-    // get(child(db, "devices/james-esp32"))
-    //   .then((s) => {
-    //     if (s.exists()) {
-    //       chart.data.datasets[0].data.push(transformData(s.val()));
-    //     }
-    //   });
-    // mockDataUpdates();
-  });
+	listenToDb();
+	onMount(() => {
+		// get(child(db, "devices/james-esp32"))
+		//   .then((s) => {
+		//     if (s.exists()) {
+		//       chart.data.datasets[0].data.push(transformData(s.val()));
+		//     }
+		//   });
+		// mockDataUpdates();
+	});
 
-  onDestroy(() => {
-    // Clean up
-    off(airQualityDataRef);
-  });
-
+	onDestroy(() => {
+		// Clean up
+		off(airQualityDataRef);
+	});
 </script>
 
 <svelte:head>
